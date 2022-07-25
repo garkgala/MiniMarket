b6c9ad3db50ddd50596d6e92bcb7ea62e2cf05d2
lbandher <leonel.lbandes@gmail.com>
2022-07-25 11:33:10 -0400

    Se diseño el formulario categorias

--- MiniMarket.presentacion/Program.cs
+++ MiniMarket.presentacion/Program.cs
@@ -16,7 +16,7 @@ namespace MiniMarket.presentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frm_Categorias());
         }
     }
 }
